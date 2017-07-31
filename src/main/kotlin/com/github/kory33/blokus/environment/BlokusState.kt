6eb053613136828a4f6094fb935ec025d7ec2e1a
Kory33 <6561358+kory33@users.noreply.github.com>
2017-07-31 21:33:32 +0200

    Update engine version / implement BlokusState

--- src/main/kotlin/com/github/kory33/blokus/environment/BlokusState.kt
+++ src/main/kotlin/com/github/kory33/blokus/environment/BlokusState.kt
@@ -1,24 +1,79 @@
 package com.github.kory33.blokus.environment
 
+import com.github.kory33.blokus.game.BlokusGame
+import com.github.kory33.blokus.game.IBlokusPlayer
+import com.github.kory33.blokus.game.color.PlayerColor
 import org.deeplearning4j.gym.StepReply
+import org.json.JSONObject
 
-class BlokusState {
-    constructor() {
-        TODO("Not implemented due to Engine API mismatch")
+class BlokusState(private val exploitingAdversary: IBlokusPlayer, private val playerColor: PlayerColor) {
+    private val blokusGame = BlokusGame()
+
+    init {
+        exploitingAdversary.assignColor(playerColor.opponentColor)
+        if (playerColor == PlayerColor.BLUE) {
+            letAdversaryPlay()
+        }
     }
 
     /**
      * Get A(s) where s = this
      */
-    fun getPossibleActions() : Set<BlokusAction> {
-        TODO("Not implemented due to Engine API mismatch")
+    fun getPossibleActions() : Set<BlokusAction> =
+            blokusGame.possiblePlacements
+                    .map{ BlokusAction(it) }
+                    .toSet()
+
+    fun letAdversaryPlay() {
+        if (this.hasGameFinished()) {
+            return
+        }
+
+        val adversaryPlacement =
+                exploitingAdversary.chooseBestPlacementFrom(blokusGame.possiblePlacements, this.blokusGame.gameData)
+        blokusGame.makePlacement(adversaryPlacement)
     }
 
-    fun hasGameFinished() : Boolean {
-        TODO("Not implemented due to Engine API mismatch")
+    fun hasGameFinished() : Boolean = blokusGame.isGameFinished
+
+    val information : JSONObject?
+            get() = null // TODO
+
+    private fun getReward() : Double {
+        if (!this.hasGameFinished()) {
+            return 0.0
+        }
+
+        val placementCounts = this.blokusGame.gameData.placementCounts.toMap()
+        val playerPlacementSize = placementCounts[playerColor]!!
+        val adversaryPlacementSize = placementCounts[playerColor.opponentColor]!!
+
+        val winningBias = if (this.blokusGame.winnerColor!! == playerColor) 10.0 else - 10.0
+
+        return playerPlacementSize - adversaryPlacementSize + winningBias
     }
+
+    private val reply = StepReply(this, this.getReward(), this.hasGameFinished(), this.information)
 
     fun step(action : BlokusAction?) : StepReply<BlokusState> {
-        TODO("Not implemented due to Engine API mismatch")
+        val validAction = action ?: return this.reply
+
+        val placement = validAction.placement
+        blokusGame.makePlacement(placement)
+
+        if (blokusGame.phase.nextPlayerColor == playerColor) {
+            return this.reply
+        }
+
+        if (blokusGame.phase.nextPlayerColor == playerColor.opponentColor) {
+            letAdversaryPlay()
+            if (blokusGame.phase.nextPlayerColor == playerColor.opponentColor) {
+                while(!this.hasGameFinished()) {
+                    letAdversaryPlay()
+                }
+            }
+        }
+
+        return this.reply
     }
 }
