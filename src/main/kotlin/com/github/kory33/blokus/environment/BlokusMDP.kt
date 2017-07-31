7dbebaa5103936716d2d84830ca2664b536b5f9c
Kory33 <6561358+kory33@users.noreply.github.com>
2017-07-31 21:45:53 +0200

    Match MDP interface with the state interface

--- src/main/kotlin/com/github/kory33/blokus/environment/BlokusMDP.kt
+++ src/main/kotlin/com/github/kory33/blokus/environment/BlokusMDP.kt
@@ -1,5 +1,7 @@
 package com.github.kory33.blokus.environment
 
+import com.github.kory33.blokus.game.IBlokusPlayer
+import com.github.kory33.blokus.game.color.PlayerColor
 import org.deeplearning4j.gym.StepReply
 import org.deeplearning4j.rl4j.learning.NeuralNetFetchable
 import org.deeplearning4j.rl4j.mdp.MDP
@@ -9,12 +11,21 @@ import org.deeplearning4j.rl4j.space.ObservationSpace
 /**
  * Blokus game represented as Markov Decision Process.
  */
-class BlokusMDP(var neuralNetFetchable: NeuralNetFetchable<IDQN>?) : MDP<BlokusState, BlokusAction, BlokusActionSpace> {
-    private var state: BlokusState = BlokusState()
+class BlokusMDP(neuralNetFetchable: NeuralNetFetchable<IDQN>,
+                private val exploitingAdversary: IBlokusPlayer,
+                private val playerColor: PlayerColor) : MDP<BlokusState, BlokusAction, BlokusActionSpace> {
+    private var state: BlokusState
 
+    private var neuralNetFetchable: NeuralNetFetchable<IDQN>
+        get() = field
+
+    init {
+        this.neuralNetFetchable = neuralNetFetchable
+        this.state = BlokusState(exploitingAdversary, playerColor)
+    }
 
     override fun reset(): BlokusState {
-        this.state = BlokusState()
+        this.state = BlokusState(exploitingAdversary, playerColor)
         return this.state
     }
 
@@ -24,7 +35,8 @@ class BlokusMDP(var neuralNetFetchable: NeuralNetFetchable<IDQN>?) : MDP<BlokusS
         return this.state.step(action)
     }
 
-    override fun newInstance(): MDP<BlokusState, BlokusAction, BlokusActionSpace> = BlokusMDP(this.neuralNetFetchable)
+    override fun newInstance(): MDP<BlokusState, BlokusAction, BlokusActionSpace>
+            = BlokusMDP(this.neuralNetFetchable, this.exploitingAdversary, this.playerColor)
 
     override fun getObservationSpace(): ObservationSpace<BlokusState> = OBSERVATION_SPACE
 
