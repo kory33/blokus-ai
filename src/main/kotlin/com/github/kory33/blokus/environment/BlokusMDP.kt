381585c1f27ab61dae40a0eecea01c2abaf2d294
Kory33 <6561358+kory33@users.noreply.github.com>
2017-08-01 00:49:30 +0200

    Do not initialize members in BlokusMDP in the constructor This is mainly because BlokusMDP depends on exploiting adversary which might again depend on BlokusMDP

--- src/main/kotlin/com/github/kory33/blokus/environment/BlokusMDP.kt
+++ src/main/kotlin/com/github/kory33/blokus/environment/BlokusMDP.kt
@@ -3,30 +3,28 @@ package com.github.kory33.blokus.environment
 import com.github.kory33.blokus.game.IBlokusPlayer
 import com.github.kory33.blokus.game.color.PlayerColor
 import org.deeplearning4j.gym.StepReply
-import org.deeplearning4j.rl4j.learning.NeuralNetFetchable
 import org.deeplearning4j.rl4j.mdp.MDP
-import org.deeplearning4j.rl4j.network.dqn.IDQN
 import org.deeplearning4j.rl4j.space.ObservationSpace
 
 /**
  * Blokus game represented as Markov Decision Process.
+ *
+ * Before starting training on this mdp, an "exploiting adversary" must be set
+ * to the corresponding field. Otherwise members in this object remains uninitialized, possibly resulting
+ * in an exception.
  */
-class BlokusMDP(neuralNetFetchable: NeuralNetFetchable<IDQN>,
-                private val exploitingAdversary: IBlokusPlayer,
-                private val playerColor: PlayerColor) : MDP<BlokusState, BlokusAction, BlokusActionSpace> {
-    private var state: BlokusState
-
-    private var neuralNetFetchable: NeuralNetFetchable<IDQN>
-        get() = field
-
-    init {
-        this.neuralNetFetchable = neuralNetFetchable
-        this.state = BlokusState(exploitingAdversary, playerColor)
-    }
+class BlokusMDP(private val playerColor: PlayerColor) : MDP<BlokusState, BlokusAction, BlokusActionSpace> {
+    lateinit var state: BlokusState
+    private var exploitingAdversary: IBlokusPlayer? = null
+        set(newAdversary) {
+            field = newAdversary
+            reset()
+        }
 
     override fun reset(): BlokusState {
-        this.state = BlokusState(exploitingAdversary, playerColor)
-        return this.state
+        val newState = BlokusState(exploitingAdversary!!, playerColor)
+        this.state = newState
+        return newState
     }
 
     override fun close() {}
@@ -35,15 +33,15 @@ class BlokusMDP(neuralNetFetchable: NeuralNetFetchable<IDQN>,
         return this.state.step(action)
     }
 
-    override fun newInstance(): MDP<BlokusState, BlokusAction, BlokusActionSpace>
-            = BlokusMDP(this.neuralNetFetchable, this.exploitingAdversary, this.playerColor)
-
-    override fun getObservationSpace(): ObservationSpace<BlokusState> = OBSERVATION_SPACE
-
     override fun getActionSpace(): BlokusActionSpace = BlokusActionSpace(this.state)
 
     override fun isDone(): Boolean = this.state.hasGameFinished()
-    
+
+    override fun newInstance(): MDP<BlokusState, BlokusAction, BlokusActionSpace>
+            = BlokusMDP(this.playerColor)
+
+    override fun getObservationSpace(): ObservationSpace<BlokusState> = OBSERVATION_SPACE
+
     companion object {
         val OBSERVATION_SPACE : ObservationSpace<BlokusState> = BlokusObservationSpace()
     }
