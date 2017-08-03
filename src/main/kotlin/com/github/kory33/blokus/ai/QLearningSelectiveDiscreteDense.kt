459eb82d56476e72bf0404567f9f8fbf88dc24d4
Kory33 <6561358+kory33@users.noreply.github.com>
2017-08-03 22:03:03 +0200

    Delete unused constructor

--- src/main/kotlin/com/github/kory33/blokus/ai/QLearningSelectiveDiscreteDense.kt
+++ src/main/kotlin/com/github/kory33/blokus/ai/QLearningSelectiveDiscreteDense.kt
@@ -3,8 +3,6 @@ package com.github.kory33.blokus.ai
 import com.github.kory33.blokus.environment.SelectiveDiscreteSpace
 import org.deeplearning4j.rl4j.learning.sync.qlearning.QLearning
 import org.deeplearning4j.rl4j.mdp.MDP
-import org.deeplearning4j.rl4j.network.dqn.DQNFactory
-import org.deeplearning4j.rl4j.network.dqn.DQNFactoryStdDense
 import org.deeplearning4j.rl4j.network.dqn.IDQN
 import org.deeplearning4j.rl4j.space.Encodable
 import org.deeplearning4j.rl4j.util.DataManager
@@ -14,17 +12,4 @@ class QLearningSelectiveDiscreteDense<O : Encodable, AS : SelectiveDiscreteSpace
          dqn : IDQN,
          conf : QLearning.QLConfiguration,
          dataManager: DataManager)
-    : QLearningSelectiveDiscrete<O, AS>(mdp, dqn, conf, dataManager, conf.epsilonNbStep) {
-
-    constructor (mdp: MDP<O, Int, AS>,
-                 factory: DQNFactory,
-                 conf: QLearning.QLConfiguration,
-                 dataManager: DataManager)
-            : this(mdp, factory.buildDQN(mdp.observationSpace.shape, mdp.actionSpace.size), conf, dataManager)
-
-    constructor (mdp: MDP<O, Int, AS>,
-                 netConf: DQNFactoryStdDense.Configuration,
-                 conf: QLearning.QLConfiguration,
-                 dataManager: DataManager)
-            : this(mdp, DQNFactoryStdDense(netConf), conf, dataManager)
-}
+    : QLearningSelectiveDiscrete<O, AS>(mdp, dqn, conf, dataManager, conf.epsilonNbStep)
